5892087adabbd0c6bb87541ad55d37056682e8b3
hkang42 <h.kang.q@gmail.com>
2020-07-10 12:03:11 -0400

    ex4. Positive and Negative

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,32 @@
+# Use dictionary keys as a set. Then solve this problem using sets.
+# Create 2 dictionaries for pos and neg numbers
+# Loop through pos dict
+#       if key * -1 is in neg dictionary:
+#            add to our list of results
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
+
+    pos = {}
+    neg = {}
+
+    for num in a:
+        if num > 0:
+            if num not in pos:
+                pos[num] = None
+        
+        elif num < 0:
+            if num not in neg:
+                neg[num] = None
+    
+    result = []
+
+    for number in pos:
+        if (number * -1) in neg:
+            result.append(number)
 
     return result
 
