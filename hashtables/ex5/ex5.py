2c4a2af1c22060e0af393dc9e6a2494d09ab0055
hkang42 <h.kang.q@gmail.com>
2020-07-10 12:20:20 -0400

    ex5. File query

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -8,6 +8,28 @@ def finder(files, queries):
     """
     # Your code here
 
+    # Loop through list of file paths and create dictionary where each key is
+    # the file (last entry in the path) and each value a list of file path(s)
+    path_dict = {}
+    for path in files:
+
+        file = path.split("/")[-1]
+        if file not in path_dict:
+            path_dict[file] = [path]
+        
+        else:
+            path_dict[file] += [path]
+
+    result = []
+
+    # Loop through every file in the query
+    #   If the file exists in our dictionary:
+    #       record the filepath(s) for that file
+    
+    for file in queries:
+        if file in path_dict:
+            result += (path_dict[file])
+
     return result
 
 
