1e43e873284be342dad2f85bab33410c6cfeb8c8
hkang42 <h.kang.q@gmail.com>
2020-07-10 13:54:48 -0400

    ex4. Added alternative solution using only 1 dictionary and a single conditional

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -31,5 +31,33 @@ def has_negatives(a):
     return result
 
 
+# Let's try writing another function
+# This time, we only use 1 dictionary
+
+# Take advantage of the fact that these 2 are effectively the same operation:
+#    neg * -1 = pos
+#    pos * -1 = neg
+
+# Regardless of whether the positive or negative value is first,
+# As long as you record the first number in the pair,
+# You can loop through the rest of the list and check to see if value * -1 matches that number
+#   If there is a match, then we have identified the pair
+
+# Once again, this function solves the problem by using the dictionary as a set. 
+# The values for the keys are irrelevant.
+
+def has_negatives2(a):
+    dict = {}
+    result = []
+    for number in a:
+        if (number * -1) not in dict:
+            dict[number] = 0
+        else:
+            result.append(abs(number))
+    return result
+
+
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+    print(has_negatives2([-1, -2, 1, 2, 3, 4, -4]))
